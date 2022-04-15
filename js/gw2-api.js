0c49a59d5d2214c919b92b15aea28f4f70c42353
unknown <N/A>
2022-04-16 00:13:26 +0300

    initial import

---
+++ js/gw2-api.js
@@ -0,0 +1,22 @@
+/*
+const ACCOUNT_TOKEN = "F86099A3-3AA5-7940-909C-F1B73D769359C641B8CF-E941-47B7-A6D0-A5DD722A49A1"
+const GUILD_ID = "7CE8522D-C709-EC11-81A8-CDE2AC1EED30"
+
+const requestURL = 'https://api.guildwars2.com/v2/account?access_token=F86099A3-3AA5-7940-909C-F1B73D769359C641B8CF-E941-47B7-A6D0-A5DD722A49A1';
+
+const guild = document.getElementsByClassName("testing")[0];
+
+
+let data;
+
+fetch(requestURL, {
+    method: 'GET',
+    headers: {
+        "Accept": "application/json"
+    }
+})
+    .then(responce => responce.json())
+    .then(responceData => {console.log(responceData); responceData => data.json() = responceData.json()})
+    //.then()
+    document.write(data)
+В ПРОЦЕССЕ*/
